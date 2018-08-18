9bf4824d48e64b85a25c64ce37dc736e74fb598e
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-08-18 19:43:12 -0300

    Inserindo uma nova categoria com post

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,23 @@
 package com.cursomc.resources;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.entity.Categoria;
 import com.cursomc.exception.ObjectNotFoundException;
 import com.cursomc.repository.CategoriaRepository;
+import com.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,8 +26,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository CategoriaRepository;
 
-	// @Autowired
-	// private CategoriaService CategoriaService;
+	@Autowired
+	private CategoriaService categoriaService;
 
 	@GetMapping("/")
 	public ResponseEntity<?> listar() {
@@ -34,6 +40,14 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objecto não encontrado id:" + id + " type:" + Categoria.class.getName())));
 
+	}
+
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Categoria categoria) {
+		Categoria cat = categoriaService.salvar(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(cat);
 	}
 
 }
