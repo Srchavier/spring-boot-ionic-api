8ec3158251a17d05b4bc3a94121a070b2d1d677a
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-08-18 22:02:28 -0300

    Paginação com parametros adicionais na requisição

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,7 +31,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 
-	@GetMapping("/")
+	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> listar() {
 		return ResponseEntity.ok().body(categoriaService.buscarTodos());
 	}
@@ -39,6 +41,16 @@ public class CategoriaResource {
 		Categoria categoria = categoriaService.buscarPorId(id);
 		return ResponseEntity.ok().body(categoria);
 
+	}
+	
+	@GetMapping("/page")
+	public ResponseEntity<Page<CategoriaDTO>> listarComPaginacao(
+			@RequestParam (value ="page", defaultValue = "0") Integer pagina, 
+			@RequestParam (value ="line", defaultValue = "24") Integer numeroLinhas, 
+			@RequestParam (value ="orderBy", defaultValue = "nome") String ordem, 
+			@RequestParam (value ="direction", defaultValue = "ASC") String direcao) {
+		
+		return ResponseEntity.ok().body(categoriaService.listaComPaginacao(pagina, numeroLinhas, ordem , direcao));
 	}
 
 	@PostMapping
--- src/main/java/com/cursomc/service/CategoriaService.java
+++ src/main/java/com/cursomc/service/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.builder.CategoriaBuilder;
@@ -17,10 +20,9 @@ import com.cursomc.service.exception.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 
-	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria salvar(Categoria categoria) {
 		return categoriaRepository.save(categoria);
 	}
@@ -45,8 +47,15 @@ public class CategoriaService {
 		List<Categoria> cat = categoriaRepository.findAll();
 		return cat.stream().map(obj -> new CategoriaBuilder().builder(obj)).collect(Collectors.toList());
 	}
-	
-	
-	
+
+	public Page<CategoriaDTO> listaComPaginacao(Integer pagina, Integer numeroLinhas, String ordem, String direcao) {
+
+		PageRequest pageRequest = PageRequest.of(pagina, numeroLinhas, Direction.valueOf(direcao), ordem);
+
+		Page<Categoria> cat = categoriaRepository.findAll(pageRequest);
+
+		return (Page<CategoriaDTO>) cat.map(obj -> new CategoriaBuilder().builder(obj));
+
+	}
 
 }
