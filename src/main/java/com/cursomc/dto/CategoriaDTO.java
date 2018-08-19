078479a764c6920400f42877f3c0c83b1e53516f
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-08-18 22:43:31 -0300

    Validação com  arquivo ValidationMessages

--- src/main/java/com/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/cursomc/dto/CategoriaDTO.java
@@ -12,8 +12,8 @@ public class CategoriaDTO implements Serializable {
 
 	private Long id;
 	
-	@NotEmpty(message = "Preenchimento obrigatório")
-	@Size(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	@NotEmpty()
+	@Size(min = 5, max = 80)
 	private String nome;
 
 	public CategoriaDTO() {
